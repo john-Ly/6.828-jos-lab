ba603a9d1b386e463b3cc022f6902b78ceeda8d4
john-Ly <cruisej9@yahoo.com>
2017-04-05 12:21:44 +0800

    ex5

--- kern/env.c
+++ kern/env.c
@@ -599,6 +599,7 @@ env_run(struct Env *e)
     curenv->env_status = ENV_RUNNING;
 	curenv->env_runs++;
 	lcr3(PADDR(curenv->env_pgdir)); // switch user address space
+    unlock_kernel();
 
     env_pop_tf(&curenv->env_tf); // restore into curenv->env_tf()
     // panic("env_run not yet implemented");
