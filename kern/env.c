b60a4e3697330b7ac1cc4023d8ed8bc116c24649
john-Ly <cruisej9@yahoo.com>
2017-04-26 15:59:17 +0800

    lab4:ipc; didn't check the pingpong & prime files; uncheck challenges

--- kern/env.c
+++ kern/env.c
@@ -320,7 +320,7 @@ env_alloc(struct Env **newenv_store, envid_t parent_id)
 	e->env_pgfault_upcall = 0;
 
 	// Also clear the IPC receiving flag.
-	e->env_ipc_recving = 0;
+	e->env_ipc_recving = false;
 
 	// commit the allocation
 	env_free_list = e->env_link;
