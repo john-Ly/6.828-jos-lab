2ef9a5666e55710df6efb60562ea62a97417befb
john-Ly <cruisej9@yahoo.com>
2017-04-30 17:32:25 +0800

    lab5 finish; without any challenge :later:

--- lib/spawn.c
+++ lib/spawn.c
@@ -129,7 +129,9 @@ spawn(const char *prog, const char **argv)
 	if ((r = copy_shared_pages(child)) < 0)
 		panic("copy_shared_pages: %e", r);
 
-	child_tf.tf_eflags |= FL_IOPL_3;   // devious: see user/faultio.c
+	// @TODO why add this line? cause spawnfaultio.c failed
+	// (what's the real purpose from this line ?)
+	// child_tf.tf_eflags |= FL_IOPL_3;   // devious: see user/faultio.c
 	if ((r = sys_env_set_trapframe(child, &child_tf)) < 0)
 		panic("sys_env_set_trapframe: %e", r);
 
@@ -321,4 +323,3 @@ copy_shared_pages(envid_t child)
 
 	return 0;
 }
-
